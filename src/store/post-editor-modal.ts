4d8695a2054c1de00baacd72691660a41d631aa7
Steady-K <tkdlxm56@gmail.com>
2026-02-05 20:47:21 +0900

    feat:(ui):implement post UI

---
+++ src/store/post-editor-modal.ts
@@ -0,0 +1,36 @@
+import { create } from "zustand";
+import { combine, devtools } from "zustand/middleware";
+
+const initialState = {
+  isOpen: false,
+};
+
+const usePostEditorModalStore = create(
+  combine(initialState, (set) => ({
+    actions: {
+      open: () => {
+        set({ isOpen: true });
+      },
+      close: () => {
+        set({ isOpen: false });
+      },
+    },
+  })),
+);
+
+export const useOpenPostEditorModal = () => {
+  const open = usePostEditorModalStore((store) => store.actions.open);
+  return open;
+};
+
+export const usePostEditorModal = () => {
+  const {
+    isOpen,
+    actions: { open, close },
+  } = usePostEditorModalStore();
+  return {
+    isOpen,
+    open,
+    close,
+  };
+};
