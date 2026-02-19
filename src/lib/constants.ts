437af33d9d46dfe6f76685b6aeb386dc2b1d8ef4
Steady-K <tkdlxm56@gmail.com>
2026-02-19 23:11:54 +0900

    feat(comment) : add comment UI and create comment

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -10,6 +10,11 @@ export const QUERY_KEYS = {
     userList: (userId: string) => ["post", "userList", userId],
     byId: (postId: number) => ["post", "byId", postId],
   },
+  commnet: {
+    all: ["comment"],
+    list: ["comment", "list"],
+    byId: (commnetId: number) => ["commnet", "byId", commnetId],
+  },
 };
 
 export const BUCKET_NAME = "uploads";
